7748b72067af6957077a6844e5ef9b0b22cb4f5c
ThndrDev <thunder.br@gmail.com>
2023-02-20 21:59:33 -0300

    Cleanup of debug stuff and small adjustment of things gravity reduction

--- WindVsGravityPatch.cs
+++ WindVsGravityPatch.cs
@@ -18,16 +18,11 @@ namespace WindVsGravity.Scripts
         {
             if (__instance is Entity)
             {
-                Debug.Log($"World Gravity: {WorldManager.CurrentWorldSetting.Gravity}");
-                Debug.Log($"Character's name:{__instance.DisplayName}  Original AtmosphereDampeningScale Value: {__instance.AtmosphereDampeningScale} ");
                 __instance.AtmosphereDampeningScale = Mathf.Clamp(0.5f + (WorldManager.CurrentWorldSetting.Gravity / 100), 0.2f, 0.5f);
-                Debug.Log($"Character's name:{__instance.DisplayName}  Patched AtmosphereDampeningScale Value: {__instance.AtmosphereDampeningScale} ");
             }
             else
             {
-            Debug.Log($"Thing Name:{__instance.DisplayName}  Original AtmosphereDampeningScale Value: {__instance.AtmosphereDampeningScale} ");
-            __instance.AtmosphereDampeningScale = Mathf.Clamp(__instance.AtmosphereDampeningScale + (WorldManager.CurrentWorldSetting.Gravity / 50), 0.1f, 1f);
-            Debug.Log($"Thing Name:{__instance.DisplayName}  New AtmosphereDampeningScale Value: {__instance.AtmosphereDampeningScale} ");
+            __instance.AtmosphereDampeningScale = Mathf.Clamp(__instance.AtmosphereDampeningScale + (WorldManager.CurrentWorldSetting.Gravity / 60), 0.3f, 1f);
             }
         }
     }
@@ -45,12 +40,10 @@ namespace WindVsGravity.Scripts
                 if (__instance.JetPackActivate)
                 {
                     __instance.ParentHuman.AtmosphereDampeningScale = 0.5f;
-                    Debug.Log($"Jetpack Activated. Human Name:{__instance.ParentHuman.DisplayName}  New AtmosphereDampeningScale Value: {__instance.ParentHuman.AtmosphereDampeningScale} ");
                 }
                 else
                 {
                     __instance.ParentHuman.AtmosphereDampeningScale = Mathf.Clamp(0.5f + (WorldManager.CurrentWorldSetting.Gravity / 100), 0.2f, 0.5f);
-                    Debug.Log($"Jetpack Deactivated. Human Name:{__instance.ParentHuman.DisplayName}  New AtmosphereDampeningScale Value: {__instance.ParentHuman.AtmosphereDampeningScale} ");
                 }
             }
         }
